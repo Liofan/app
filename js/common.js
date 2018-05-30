96b183ce1c1082ee65b2e05d599dd0763ebe5c35
liofan <jakanov.andr@gmail.com>
2018-05-31 01:25:28 +0600

    e

--- js/common.js
+++ js/common.js
@@ -2,4 +2,12 @@ $(function() {
 
 	// Custom JS
 
+	$('.countdown').downCount({
+		date: deadline,
+	},
+	function(){
+		 /* действие после завершения таймера */
+		 alert("Время истекло!");
+	});
+
 });
